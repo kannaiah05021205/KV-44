46bd90661b867167b15a8c0d70b30c955f72982a
kannaiah05021205 <kannaiah05021205@gmail.com>
2026-08-08 17:17:00 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
+  output: 'export',
+  images: { unoptimized: true },
+  basePath: '/KV-44',
 };
 
 export default nextConfig;
